c883f2a4fe44bfe2d89eca532e75f4364c0a53b7
berth21gg <19031032@itcelaya.edu.mx>
2024-05-15 17:25:15 -0600

    Implementación de botón con showDialog para marcar estado

--- lib/pages/rent_details_page.dart
+++ lib/pages/rent_details_page.dart
@@ -68,20 +68,89 @@ class _RentDetailsPageState extends State<RentDetailsPage> {
                   borderRadius: BorderRadius.circular(5),
                 ),
               ),
-              onPressed: () {},
-              child: const Text('Cancelar'),
-            ),
-          ),
-          const SizedBox(width: 20),
-          Expanded(
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5),
-                ),
-              ),
-              onPressed: () {},
-              child: const Text('Completar'),
+              onPressed: () {
+                String? state;
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: const Text('Marcar renta como'),
+                    content: SingleChildScrollView(
+                      child: StatefulBuilder(
+                        builder: (context, setState) {
+                          return Column(
+                            children: [
+                              ListTile(
+                                title: Text('Por cumplir'),
+                                leading: Radio(
+                                  value: "Por cumplir",
+                                  groupValue: state,
+                                  onChanged: (String? selectedState) {
+                                    setState(() {
+                                      state = selectedState;
+                                    });
+                                  },
+                                ),
+                              ),
+                              ListTile(
+                                title: Text('En proceso'),
+                                leading: Radio(
+                                  value: "En proceso",
+                                  groupValue: state,
+                                  onChanged: (String? selectedState) {
+                                    setState(() {
+                                      state = selectedState;
+                                    });
+                                  },
+                                ),
+                              ),
+                              ListTile(
+                                title: Text('Cumplida'),
+                                leading: Radio(
+                                  value: "Cumplida",
+                                  groupValue: state,
+                                  onChanged: (String? selectedState) {
+                                    setState(() {
+                                      state = selectedState;
+                                    });
+                                  },
+                                ),
+                              ),
+                              ListTile(
+                                title: Text('Cancelada'),
+                                leading: Radio(
+                                  value: "Cancelada",
+                                  groupValue: state,
+                                  onChanged: (String? selectedState) {
+                                    setState(() {
+                                      state = selectedState;
+                                    });
+                                  },
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      ),
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text('Abortar'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          print(state);
+                          Navigator.pop(context);
+                        },
+                        child: const Text('Confirmar'),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              child: const Text('Marcar como'),
             ),
           ),
         ],
